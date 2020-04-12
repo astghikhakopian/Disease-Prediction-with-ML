1871449a41602917ae005d1377c501efdfdf84f0
astghikhakopian <astghik.hakopian@gmail.com>
2020-04-13 00:39:09 +0400

    setup.

---
+++ web frontend/app.js
@@ -0,0 +1,15 @@
+$("document").ready(function(){
+    $("#send").click(function(){
+        var symptoms = $("#symptoms").val();
+        $.ajax({
+            url: "http://192.168.0.104:5000/",
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: { "symptoms": symptoms }
+        }).done(function(data) {
+            $("#disease").html(data['data']);
+            // console.log(data);
+        });
+    });
+});
